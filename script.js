8fafd8115402c872d5fba42354985d8c7d22f68a
Robert Boespflug <boespflr@oregonstate.edu>
2020-10-18 14:12:11 -0700

    text content set on board, no events

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 const board = document.querySelector('#board')
 
 //loop
-for (let i = 0; i < 9; i++) {
-    //make element
-    const div = 
-    document.createElement("DIV")
-    div.setAttribute("class", "tactoes")
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        //make element
+        const div = 
+        document.createElement("DIV")
+        div.setAttribute("class", "tactoes")
 
-    const h1 = document.createElement('H1')
-    h1.textContent = i + 1
-    div.appendChild(h1)
-    board.appendChild(div)
+        const h1 = document.createElement('H1')
+        h1.textContent = `${i + 1}, ${j+1}`
+        div.appendChild(h1)
+        board.appendChild(div)
+    }
 }
 
-//insert into board
+//events
