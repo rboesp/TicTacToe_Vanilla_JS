a3b272954e34540cf49080c492dd69b92d4a02ab
Robert Boespflug <boespflr@oregonstate.edu>
2020-10-21 19:52:25 -0700

    Added game manager class

--- script.js
+++ script.js
@@ -16,45 +16,70 @@ class Tile {
     }
 }
 
-const tileStates = {
-    0: new Tile(0,0),
-    1: new Tile(0,1),
-    2: new Tile(0,2),
-    3: new Tile(1,0),
-    4: new Tile(1,1),
-    5: new Tile(1,2),
-    6: new Tile(2,0),
-    7: new Tile(2,1),
-    8: new Tile(2,2)
-}
-
 const players = [{
     color: 'red'
 }, {
     color: 'blue'
 }]
 
+class GameManager{
+    constructor() {
+        this.currentPlayer = 0
+        this.winner = null
+        this.tileStates = {
+            0: new Tile(0,0),
+            1: new Tile(0,1),
+            2: new Tile(0,2),
+            3: new Tile(1,0),
+            4: new Tile(1,1),
+            5: new Tile(1,2),
+            6: new Tile(2,0),
+            7: new Tile(2,1),
+            8: new Tile(2,2)
+        }
+        this.moves = 0
+    }
+
+    increaseMoves() {
+        this.moves++
+    }
+
+    checkForWinner() {
+        if(this.moves < 5) return
+        //here find three in a row
+        return 'blue wins!'
+    }
+
+    nextPlayer() {
+        return Number(this.currentPlayer = !this.currentPlayer)
+    }
+}
+
 const board = document.querySelector('#board')
-let currentPlayer = 0
+const ticTacToe = new GameManager()
+const winnerEl = document.querySelector('#winner')
 
 //FUNCTIONS
 function markTile(tile, color) {
     tile.setAttribute('style', `background-color: ${color};`)
 }
 
-function nextPlayer() {
-    return Number(currentPlayer = !currentPlayer)
-}
+// function nextPlayer() {
+//     return Number(currentPlayer = !currentPlayer)
+// }
 
 function handleClick(e) {
     if(e.target.className !== 'tiles') return
-    const tileState = tileStates[e.target.id]
-    const takenTile = tileState.take()
+    const tile = ticTacToe.tileStates[e.target.id]
+    const takenTile = tile.take()
     if(!takenTile) return
-    let player = nextPlayer()
+    let player = ticTacToe.nextPlayer()
     console.log(player);
-    console.log(tileState);
+    console.log(tile);
     markTile(e.target, players[player].color)
+    ticTacToe.increaseMoves()
+    const winner = ticTacToe.checkForWinner()
+    if(winner) winnerEl.textContent = winner
 }
 
 //EVENTS
@@ -77,11 +102,15 @@ for (let i = 0; i < 3; i++) {
         //make tile
         const div = document.createElement("DIV")
         div.setAttribute("class", "tiles")
-        div.setAttribute("id", `${counter++}`)
+        div.setAttribute("id", `${counter}`)
 
         const h1 = document.createElement('H1')
-        h1.textContent = `${i+1},${j+1}`
+        h1.textContent = `${counter++}`
         div.appendChild(h1)
         board.appendChild(div)
     }
+}
+
+const pathsToWin = {
+    
 }
